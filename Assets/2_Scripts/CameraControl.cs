b8b38a5dc524945d73c1678f13d4a1d8787fa3d7
FernandoHazel <A01229544@itesm.mx>
2023-02-01 17:59:21 -0600

    arreglados ángulos de cámara

--- Assets/2_Scripts/CameraControl.cs
+++ Assets/2_Scripts/CameraControl.cs
@@ -8,6 +8,8 @@ public class CameraControl : MonoBehaviour
     public float transitionSpeed = 1f;
     public Transform currentView;
 
+    private bool canMove = false;
+
     private void Start()
     {
         currentView = views[0];
@@ -16,11 +18,35 @@ public class CameraControl : MonoBehaviour
 
     private void LateUpdate()
     {
+        //We only moves the camera if it is not in the current view position
+        CheckPosition();
+
+        if (canMove)
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        //Move the camera from its actual position to the new view position 
+        //With a smooth motion
         transform.position = Vector3.Lerp(transform.position, currentView.position, (Time.deltaTime * transitionSpeed));
         Vector3 currentAngle = new Vector3(
          Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
          Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
          Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
         transform.eulerAngles = currentAngle;
+    }
+
+    private void CheckPosition()
+    {
+        //When we reach the target we stop the camera movement
+        if (transform.position == currentView.position)
+        {
+            canMove = false;
+        }
+        else
+        {
+            canMove = true;
+        }
     }
 }
