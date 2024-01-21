2100829bc713015947a04338214bc341714342be
Fernando <fernandohazel1@gmail.com>
2024-01-21 10:33:52 -0600

    added readme

--- Assets/2_Scripts/PauseAnimationsManager.cs
+++ Assets/2_Scripts/PauseAnimationsManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PauseAnimationsManager : MonoBehaviour
 {
    Animator pauseAnimator = default; //we take the animator of the pauseScreen
-
    public void PauseAnimator()
    {
        pauseAnimator.SetTrigger("Play"); //we set a trigger to swhich between pause animations
---
+++ README.md
@@ -0,0 +1,8 @@
+# Not So Bee-zy
+
+This is a little casual game about a bee stealig pollen
+
+The goal is to collect all the pollen of each level withou been captured
+
+# Unity version
+2022.3.5.f1
