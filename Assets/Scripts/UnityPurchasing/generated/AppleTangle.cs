0b6ae40edb6ca310b36435004af974464b84042a
Fernando Ascencio <fernando.ascencio@bradesco.com.mx>
2024-07-04 12:55:37 -0600

    iphone only

---
+++ Assets/Scripts/UnityPurchasing/generated/AppleTangle.cs
@@ -0,0 +1,18 @@
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    public class AppleTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("MXv8FGFd64BE9sC7Vy2xdvdw5EcElgZRdsTjeogkrb+NZ5exd9+GXKOv7Or9++bp5uzu++qv/+Dj5uz2++bp5uzu++qv7fav7uH2r//u/fvWKIqG85jP2Z6R+1w4BKy0yCxa4OPqr8bh7KG+qb+riYzai4Scks7/mb+biYzai4ycgs7//+Pqr93g4Pvd6uPm7uHs6q/g4a/75+b8r+zq/YekiY6KioiNjpmR5/v7//y1oKD4r+7h66/s6v375unm7O775uDhr///4+qv3eDg+6/Mzr+RmIK/ub+7vby51b/tvoS/homM2ouJnI3a3L6c6ACHO694RCOjr+D/ObCOvwM4zEBWufBOCNpWKBY2vc10V1r+EfEu3a/Mzr8Njq2/gomGpQnHCXiCjo6OAPwO70mU1IagHT13y8d/77cRmnokLP4dyNzaTiCgzjx3dGz/Qmkswz6/12PVi70D5zwAklHq/HDo0eozi4mcjdrcvpy/nomM2ouFnIXO//+JjNqSgYuZi5ukX+bIG/mGcXvkAqttZF44/1CAym6oRX7i92JoOpiYib+AiYzakpyOjnCLir+Mjo5wv5Kpv6uJjNqLhJySzv//4+qvzOr9+6UJxwl4go6OioqPv+2+hL+GiYzaxlf5ELyb6i74G0aijYyOj44sDY65FsOi9zhiAxRTfPgUffld+L/ATv/j6q/M6v375unm7O775uDhr876D5ukX+bIG/mGcXvkAqHPKXjIwvCQHlSRyN9kimLR9guiZLkt2MPaY7+eiYzai4Wchc7//+Pqr8bh7KG+sqnorwW85XiCDUBRZCygdtzl1OuKj4wNjoCPvw2OhY0Njo6Pax4mhq/g6a/75+qv++fq4a/u///j5uzuur2+u7+8udWYgry6v72/tr2+u784lDIczaudpUiAkjnCE9HsR8QPmPW/DY75v4GJjNqSgI6OcIuLjI2O67qsmsSa1pI8G3h5ExFA3zVO1986tSJ7gIGPHYQ+rpmh+1qzglTtmYfRvw2OnomM2pKviw2Oh78Njou/oL8OTImHpImOioqIjY2/DjmVDjz97uz75uzqr/z77vvq4urh+/yhvxoR9YMryATUW5m4vERLgMJBm+Zevw2LNL8NjCwvjI2OjY2Ojb+CiYaCiYalCccJeIKOjoqKj4wNjo6P0ydT8a26RapaVoBZ5Fstq6yeeC4j9q/u/Pz64ur8r+7s7Or/++7h7OqQCgwKlBayyLh9JhTPAaNbPh+dV0/svPh4tYij2WRVgK6BVTX8lsA6DY6PiYalCccJeOzrio6/Dn2/pYn4+KHu///j6qHs4OKg7v//4+rs7ubp5uzu++bg4a/O+vvn4P3m+/a+7ePqr/z77uHr7v3rr/vq/eL8r+7h66/s4OHr5vvm4OH8r+Dpr/r86ohj8rYMBNyvXLdLPjAVwIXkcKRzoc8peMjC8IfRv5CJjNqSrIuXv5n75+D95vv2vpm/m4mM2ouMnILO/0aW/XrSgVrw0BR9qow12gDC0oJ+yvGQw+TfGc4GS/vthJ8Mzgi8BQ7wzicXdl5F6ROr5J5fLDRrlKVMkIASsnykxqeVR3FBOjaBVtGTWUSy3yUFWlVrc1+GiLg/+vqu");
+        private static int[] order = new int[] { 50,16,44,39,52,39,12,38,35,45,12,35,21,58,58,55,24,26,44,30,53,29,29,24,27,48,52,32,55,45,56,58,41,38,59,55,53,41,38,41,46,44,50,47,54,46,49,57,59,53,55,55,57,55,58,58,56,59,59,59,60 };
+        private static int key = 143;
+
+        public static readonly bool IsPopulated = true;
+
+        public static byte[] Data() {
+        	if (IsPopulated == false)
+        		return null;
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
